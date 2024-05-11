abd480b4173fc2c8ae7385387136c503ded4afa5
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2024-05-11 19:23:11 +0530

    project init

---
+++ EmojiLover/EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by shubham.dhage on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
